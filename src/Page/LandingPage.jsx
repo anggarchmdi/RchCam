19c1965853eef11f902f81f39dac86acbfb56124
anggarchmdi <anggarchmdi@gmail.com>
2024-10-09 15:40:25 +0700

    dasar dasar tampilan

--- src/Page/LandingPage.jsx
+++ src/Page/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function LandingPage() {
   return (
-    <div>
-      
+    <div className='bg-slate-900 w-full h-screen'>
+<Navbar />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import LogoImg from '../assets/Logo.png'
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className='w-full container mx-auto shadow-md'>
+    <div className="p-2 grid grid-cols-2">
+        <div className="flex justify-start">
+            {/* Logo */}
+            <div className="rounded-full w-10 h-10 flex gap-2 items-center">
+                <img src={LogoImg} alt="Logo" className='w-full bg-cover rounded-full' />
+                <h1 className='text-[1.3rem] font-semibold text-white'>RchCamera</h1>
+            </div>
+        </div>
+            {/* Button */}
+            <div className="flex justify-end ml-auto items-center gap-4 mr-4">
+               <button className='text-white transition-all transform hover:scale-90 duration-300 text-[1.1rem]'>Login</button>
+               <span className='bg-white h-8 w-0.5'></span>
+               <button className='text-white transition-all transform hover:scale-90 duration-300 text-[1.1rem]'>Daftar</button>
+            </div>
+    </div>
     </div>
   )
 }
