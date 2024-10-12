faca5205bbb40d86aac6abd122c8657c86c4bb38
anggarchmdi <anggarchmdi@gmail.com>
2024-10-12 10:34:32 +0700

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-slate-900 bottom-0 flex justify-center p-4 text-white border-t-2 border-slate-800'>
+    <div className='w-screen overflow-x-hidden bg-slate-900 bottom-0 flex justify-center p-4 text-white border-t-2 border-slate-800'>
       <h1 className='font-thin text-[0.9rem]'>copyright 2024 | RchCamera Official</h1>
     </div>
   )
